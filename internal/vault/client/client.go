e659655eebf14791ceaadbda81ef863628e8cdff
Ben Fiola <benfiola@gmail.com>
2025-12-11 23:17:24 -0800

    feat: remove work related to and including vault-init

--- internal/vault/client/client.go
+++ internal/vault/client/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 	"os/exec"
 
 	"github.com/benfiola/homelab-helper/internal/process"
@@ -31,116 +30,6 @@ func New(opts *Opts) (*Client, error) {
 		Token:   opts.Token,
 	}
 	return &client, nil
-}
-
-type AuthEngine struct{}
-
-func (c *Client) ListAuthEngines(ctx context.Context) (map[string]AuthEngine, error) {
-	output, err := process.Output(ctx, []string{"vault", "auth", "list", "--format=json"})
-	if err != nil {
-		return nil, err
-	}
-
-	data := map[string]AuthEngine{}
-	err = json.Unmarshal([]byte(output), &data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-func (c *Client) EnableAuthEngine(ctx context.Context, engine string) error {
-	_, err := process.Output(ctx, []string{"vault", "auth", "enable", engine})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *Client) ListPolicies(ctx context.Context) ([]string, error) {
-	output, err := process.Output(ctx, []string{"vault", "policy", "list", "--format=json"})
-	if err != nil {
-		return nil, err
-	}
-
-	data := []string{}
-	err = json.Unmarshal([]byte(output), &data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-func (c *Client) CreatePolicy(ctx context.Context, name string, content string) error {
-	file, err := os.CreateTemp("", "")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	defer os.Remove(file.Name())
-
-	_, err = file.WriteString(content)
-	if err != nil {
-		return err
-	}
-
-	_, err = process.Output(ctx, []string{"vault", "policy", "write", name, file.Name()})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-type SecretEngine struct{}
-
-func (c *Client) ListSecretEngines(ctx context.Context) (map[string]SecretEngine, error) {
-	output, err := process.Output(ctx, []string{"vault", "secrets", "list", "--format=json"})
-	if err != nil {
-		return nil, err
-	}
-
-	data := map[string]SecretEngine{}
-	err = json.Unmarshal([]byte(output), &data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-type KV2 struct {
-	Description string
-	Path        string
-	Version     string
-}
-
-func (c *Client) EnableSecretEngine(ctx context.Context, engine any) error {
-	command := []string{"vault", "secrets", "enable"}
-
-	if kv2, ok := engine.(KV2); ok {
-		version := kv2.Version
-		if version == "" {
-			version = "2"
-		}
-
-		command = append(command, fmt.Sprintf("--path=%s", kv2.Path))
-		command = append(command, fmt.Sprintf("--description=%s", kv2.Description))
-		command = append(command, fmt.Sprintf("--version=%s", version))
-		command = append(command, "kv2")
-	} else {
-		return fmt.Errorf("unimplemented")
-	}
-
-	_, err := process.Output(ctx, command)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 type Status struct {
@@ -184,17 +73,4 @@ func (c *Client) Read(ctx context.Context, path string) (string, error) {
 	}
 
 	return output, err
-}
-func (c *Client) Write(ctx context.Context, path string, data map[string]string) error {
-	command := []string{"vault", "write", path}
-	for k, v := range data {
-		command = append(command, fmt.Sprintf("%s=%s", k, v))
-	}
-
-	_, err := process.Output(ctx, command)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
--- internal/vault/initialize/policy.template.hcl
+++
@@ -1,3 +0,0 @@
-path '%s/*' {
-  capabilities = ['read', 'list']
-}
