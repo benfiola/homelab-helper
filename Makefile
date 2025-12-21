123ea1ab4e6a389c4e4051a1e4a13e8022cec6a8
Ben Fiola <benfiola@gmail.com>
2025-12-21 00:05:18 -0800

    fix: add more helm chart plumbing

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CONTROLLERGEN_VERSION := 0.20.0
 GORELEASER_VERSION := 2.12.7
+HELM_VERSION := 4.0.4
 SVU_VERSION := 3.3.0
 
 include Makefile.include.mk
@@ -39,6 +40,10 @@ ifeq ($(goreleaser_arch),amd64)
 endif
 goreleaser_url := https://github.com/goreleaser/goreleaser/releases/download/v$(GORELEASER_VERSION)/goreleaser_Linux_$(goreleaser_arch).tar.gz
 $(eval $(call tool-from-tar-gz,goreleaser,$(goreleaser_url),0))
+
+helm_arch := $(arch)
+helm_url := https://get.helm.sh/helm-v$(HELM_VERSION)-linux-$(helm_arch).tar.gz
+$(eval $(call tool-from-tar-gz,helm,$(helm_url),1))
 
 svu_url := https://github.com/caarlos0/svu/releases/download/v$(SVU_VERSION)/svu_$(SVU_VERSION)_linux_$(arch).tar.gz
 $(eval $(call tool-from-tar-gz,svu,$(svu_url),0))
---
+++ scripts/set-helm-version/main.go
@@ -0,0 +1,54 @@
+package sethelmversion
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/goccy/go-yaml"
+)
+
+func Run() error {
+	if len(os.Args) < 3 {
+		return fmt.Errorf("usage: set-helm-version [chart-path] [version]")
+	}
+	chartPath := os.Args[1]
+	version := os.Args[2]
+
+	file := filepath.Join(chartPath, "Chart.yaml")
+	contents, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	data := map[string]any{}
+	err = yaml.Unmarshal(contents, &data)
+	if err != nil {
+		return err
+	}
+
+	data["version"] = version
+
+	contents, err = yaml.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(file, contents, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	err := Run()
+
+	code := 0
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		code = 1
+	}
+	os.Exit(code)
+}
